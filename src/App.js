3ebb419eea09d06b2b6d36f6f360ecafb4086f07
Rohit kalase <85866902+Rohit29-rk@users.noreply.github.com>
2022-07-03 15:30:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Footer from './component/Footer';
 function App() {
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var mm = String(today.getMonth() + 1).padStart(2, "0");
   var yyyy = today.getFullYear();
   today = yyyy + "-" + mm + "-" + dd;
  
@@ -30,7 +30,8 @@ function App() {
           
           
           
-<Switch><Route exact path="/"><Card key="top" match="Current Matches" category="results-by-date"/></Route>
+            <Switch>
+            <Route exact path="/"><Card key="top" match="Matches" category="results-by-date"/></Route>
             <Route exact path="/recent"><Card  key="result" match="Recent Matches" category="results" /></Route>
             <Route exact path="/upcoming"><Card key="fixture" match="Fixtures" category="fixtures"  /></Route>
             <Route exact path="/news"><News key="News"/></Route>
